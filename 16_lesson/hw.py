d454a063d3461cfe4d4ce29ac405a7afbda97bfb
Iliyana Ilieva <iliyana.ilieva@mycomplianceoffice.com>
2024-09-03 16:46:56 +0300

    16 lesson hw

---
+++ 16_lesson/hw.py
@@ -0,0 +1,22 @@
+"""
+EXERCISE 2
+In the GE 30424 ENG - Your_Student_ID.py file there’s the Python custom function create_dict which task is to create a dictionary 
+in which the keys are integers and the values are randomly picked from the elements of a sequence. The function is incomplete and must:
+
+have as a mandatory parameter a sequence containing a series of text strings from which to randomly extract the values
+have as an optional parameter the number of dictionary elements to be created. In the absence of an argument passed to the function, the default value should be 4
+use a loop controlled by a condition to create a dictionary in which the keys are integers starting from 1, whereas the values are randomly picked from the sequence of strings 
+passed as a mandatory argument. Note: The dictionary values must be without duplicates
+return the created dictionary to the calling instruction
+"""
+
+"""
+Write a function does_contain_upper that receives a word and returns True if the word contains at least one uppercase letter, and False otherwise.
+Write a program that asks the user for a word and then uses the does_contain_upper function to print "Valid word" if the word contains at least one upper letter and "Invalid word" otherwise.
+"""
+
+"""
+Write a program that asks the user for a school subject (out of English, Math or Biology) and then for a grade, and saves the grades in a list for this subject.
+Also save the average grade for this subject in a dictionary where the key is the school subject and the value is the average grade.
+Print the average grade for all subjects after the user decides to stop the program by pressing Enter (empty input).
+"""
